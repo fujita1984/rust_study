617cf86f06fa405f651f37e2acbfe08b10f54f6f
fujita <fujita1110@eos.ocn.ne.jp>
2024-08-04 00:49:42 +0900

    array

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 //別ファイルの関数を利用するためにmod filenameとする;
+mod array;
 mod variable;
 
 fn main() {
     variable::var_num();
     variable::var_string();
     variable::var_str();
+    array::arrays();
 }
